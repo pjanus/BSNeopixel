4f7f119bd8b639f9913b2751165b7e965699c2ed
pjanus <pawel92janus@gmail.com>
2015-06-22 17:54:22 +0200

    [Examples] Equalizer gradient

--- examples/Equalizer/Equalizer.ino
+++ examples/Equalizer/Equalizer.ino
@@ -41,13 +41,28 @@ void loop() {
     // for (int row = 0; row < 5; row++) {
     //     bs.setRowHeight(row, rows[row]+1, COLORS[row]);
     // }
+
     for (int row = 0; row < 5; row++) {
         for (int i = 0; i < rows[row]+1; i++) {
-            uint32_t color = BLUE;
-            if (i > 20) color = YELLOW;
-            if (i > 31) color = RED; 
-            bs.setPixelColor(row, i, color);
+            // tricolors(row, i);
+            gradient(row, i);
         }
     }
+
     bs.show();
+}
+
+void tricolors(int row, int i)
+{
+    uint32_t color = BLUE;
+    if (i > 20) color = YELLOW;
+    if (i > 31) color = RED; 
+    bs.setPixelColor(row, i, color);
+}
+
+void gradient(int row, int i)
+{
+    uint8_t red = map(i, 0, 39, 0, 128);
+    uint8_t green = map(i, 0, 39, 128, 0);
+    bs.setPixelColor(row, i, bs.Color(red, green, 0));
 }
